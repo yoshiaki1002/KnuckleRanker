777214f7e916dd1fa12302d53c349ef77be707e7
Yoshiaki Hiroi <yoshiaki.hiroi1002@gmail.com>
2020-09-13 14:47:40 +0900

    add Xcode project for WatchKit App

---
+++ KnuckleRanker WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  KnuckleRanker WatchKit Extension
+//
+//  Created by 広井 芳秋 on 2020/09/05.
+//  Copyright © 2020 Yoshiaki Hiroi. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ KnuckleRanker WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  KnuckleRanker WatchKit Extension
+//
+//  Created by 広井 芳秋 on 2020/09/05.
+//  Copyright © 2020 Yoshiaki Hiroi. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
